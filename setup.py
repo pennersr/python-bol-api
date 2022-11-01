a4a64567f95fa1490e689c7d327d0a569b95fb5a
Raymond Penners <raymond.penners@intenct.nl>
2022-11-01 21:37:59 +0100

    chore(setup): Remove py2

--- setup.py
+++ setup.py
@@ -1,47 +1,45 @@
 import sys
-from setuptools import setup, find_packages
+
+from setuptools import find_packages, setup
 
 import bol
 
+
 # Dynamically calculate the version based on bol.VERSION.
 VERSION = bol.__version__
-IS_PY2 = sys.version_info[0] < 3
 
-install_requires = [
-    'python-dateutil',
-    'requests']
-if IS_PY2:
-    install_requires.append('enum34')
+install_requires = ["python-dateutil", "requests"]
 
-setup(name='python-bol-api',
-      version=VERSION,
-      description="Wrapper for the bol.com API",
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'Intended Audience :: Developers',
-          'Intended Audience :: System Administrators',
-          'Operating System :: OS Independent',
-          'Topic :: Software Development',
-          'Topic :: System',
-          'Topic :: System :: Software Distribution',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7',
-          'Programming Language :: Python :: 3.8',
-          'License :: OSI Approved :: '
-          'GNU Lesser General Public License v3 or later (LGPLv3+)',
-      ],
-      keywords='bol bol.com api wrapper',
-      author='Raymond Penners',
-      author_email='raymond.penners@intenct.nl',
-      url='http://www.intenct.info/',
-      license='',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=install_requires,
-      entry_points="")
+setup(
+    name="python-bol-api",
+    version=VERSION,
+    description="Wrapper for the bol.com API",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+        "Topic :: System",
+        "Topic :: System :: Software Distribution",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: "
+        "GNU Lesser General Public License v3 or later (LGPLv3+)",
+    ],
+    keywords="bol bol.com api wrapper",
+    author="Raymond Penners",
+    author_email="raymond.penners@intenct.nl",
+    url="http://www.intenct.info/",
+    license="",
+    packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points="",
+)
